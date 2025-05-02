04a1d99104e8b019a5e057497a0c78e97df39194
martinsevandro <evandro.filho@outlook.com.br>
2025-05-02 15:31:41 -0300

    add flip-card frontcard-backcard(ugo) and vanillaTilt

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 document.getElementById('search-form').addEventListener('submit', async (e) => {
     e.preventDefault();
-  
+
     const name = document.getElementById('riot-name').value.trim();
     const tag = document.getElementById('riot-tag').value.trim();
     const server = document.getElementById('riot-server').value.trim();
-  
+
     if (!name || !tag) return alert('Preencha os dois campos!');
-  
+
     try {
         console.log(`Buscando PUUID para: ${name} ${tag} ${server}`);  // Adicione este log
 
@@ -39,17 +39,17 @@ document.getElementById('search-form').addEventListener('submit', async (e) => {
         alert('Erro ao buscar dados. Verifique o nome e tag ou tente novamente.');
     }
 });
-  
+
 function renderCard(data) {
     const container = document.getElementById('card-container');
     container.innerHTML = ''; // Limpar o container
-  
+
     const card = document.createElement('div');
-    card.classList.add('player-card');
+    card.classList.add('player-card', 'flip-card', 'w-[308px]', 'h-[560px]', 'perspective');
 
     let cardContent = "";
 
-    if (data.realRole === "sup" ) {
+    if (data.realRole === "sup") {
         cardContent = `
             <!-- Estatisticas gerais - Primeira linha -->
             <div class="flex justify-between text-sm text-gray-200 font-medium">
@@ -148,89 +148,118 @@ function renderCard(data) {
     if (data.totalHealsOnTeammates >= 10000 & data.killParticipation >= 30.0) {
         achievements.push(`<img src="../assets/achievements/challenge-healer.png" class="w-6 h-6" alt="achievement-HealsOnTeammates">`);
     }
-  
+
     card.innerHTML = `
-        <div class="relative w-[308px] h-[560px] rounded-none overflow-hidden shadow-lg border-4" style="border-color: ${data.corDaBorda};">
-            <!-- Splash da skin de fundo -->
-            <img src="${data.splashArt}" 
-            class="absolute w-full h-full object-cover" 
-            alt="card-champion" />
-            
-            <!-- Conteúdo que ficará sobre o card -->
-            <div class="absolute bottom-0 w-full text-white p-4 space-y-2">
+        <div class="carta flip-card w-[308px] h-[560px] relative">
+        
+            <div class="flip-inner relative w-full h-full">
 
-                <!-- Nome e Tag -->
-                <div class="flex justify-center">
-                    <div class=" p-2 rounded-md text-center">
-                        <h2 class="text-blur text-6xl" style="
-                        font-family: 'Herr Von Muellerhoff', cursive;
-                        color: white;  -webkit-text-stroke: 0.5px gold;">
-                        ${data.riotIdGameName}</h2>
-                    </div>
-                </div>
-                       
-                <!-- Estatisticas gerais - Primeira, Segunda e Terceira linhas -->
-                ${cardContent}
+                <!-- Frente do card -->
+                <div class="flip-front relative w-full h-full rounded-none overflow-hidden shadow-lg border-4 " style="border-color: ${data.corDaBorda};">
                 
-
-
-                <!-- Runas e StatPerks - Quarta linha -->
-                <div class="flex justify-between items-center mt-2">
-                    <!-- Runas -->
-                    <div class="flex flex-wrap gap-1">
-                        <img src="${data.perks.primaryStyle}" class="w-6 h-6" alt="runap0">
-                        <img src="${data.perks.primaryStyleSec}" class="w-6 h-6" alt="runap1">
-                        <img src="${data.perks.primaryStyleTert}" class="w-6 h-6" alt="runap2">
-                        <img src="${data.perks.primaryStyleQuat}" class="w-6 h-6" alt="runap3">
-                        <img src="${data.perks.subStyle}" class="w-6 h-6" alt="runas0">
-                        <img src="${data.perks.subStyleSec}" class="w-6 h-6" alt="runas1">
-                    </div>
-
-                    <!-- StatPerks -->
-                    <div class="flex gap-1 ml-2">
-                        <img src="${data.perks.offense}" class="w-6 h-6" alt="offense-perk">
-                        <img src="${data.perks.flex}" class="w-6 h-6" alt="flex-perk">
-                        <img src="${data.perks.defense}" class="w-6 h-6" alt="defense-perk">
-                    </div>
-                </div>
-
-                <!-- Itens e Spells - Quinta linha -->
-                <div class="flex justify-between items-center mt-2">
-                    <!-- Itens -->
-                    <div class="flex flex-wrap gap-1">
-                        ${data.items
-                        .filter(id => id !== 0) // filtra itens "vazios"
-                        .map(id => `<img src="https://ddragon.leagueoflegends.com/cdn/15.8.1/img/item/${id}.png" class="w-6 h-6" alt="item ${id}">`)
-                        .join('')}
-                    </div>
-
-                    <!-- Spells -->
-                    <div class="flex gap-1 ml-2">
-                        <img src="${data.summonerSpells.spell1}" class="w-6 h-6 bg-black/25 backdrop-blur-sm" alt="spell1">
-                        <img src="${data.summonerSpells.spell2}" class="w-6 h-6 bg-black/25 backdrop-blur-sm" alt="spell2">
-                    </div>                    
+                    <!-- Splash da skin de fundo -->
+                    <img src="${data.splashArt}" 
+                    class="absolute w-full h-full object-cover" 
+                    alt="card-champion" />
                     
+                    <!-- Conteúdo que ficará sobre o card -->
+                    <div class="absolute bottom-0 w-full text-white p-4 space-y-2">
+
+                        <!-- Nome e Tag -->
+                        <div class="flex justify-center">
+                            <div class=" p-2 rounded-md text-center">
+                                <h2 class="text-blur text-6xl" style="
+                                font-family: 'Herr Von Muellerhoff', cursive;
+                                color: white;  -webkit-text-stroke: 0.5px gold;">
+                                ${data.riotIdGameName}</h2>
+                            </div>
+                        </div>
+                            
+                        <!-- Estatisticas gerais - Primeira, Segunda e Terceira linhas -->
+                        ${cardContent}
+                        
+
+
+                        <!-- Runas e StatPerks - Quarta linha -->
+                        <div class="flex justify-between items-center mt-2">
+                            <!-- Runas -->
+                            <div class="flex flex-wrap gap-1">
+                                <img src="${data.perks.primaryStyle}" class="w-6 h-6" alt="runap0">
+                                <img src="${data.perks.primaryStyleSec}" class="w-6 h-6" alt="runap1">
+                                <img src="${data.perks.primaryStyleTert}" class="w-6 h-6" alt="runap2">
+                                <img src="${data.perks.primaryStyleQuat}" class="w-6 h-6" alt="runap3">
+                                <img src="${data.perks.subStyle}" class="w-6 h-6" alt="runas0">
+                                <img src="${data.perks.subStyleSec}" class="w-6 h-6" alt="runas1">
+                            </div>
+
+                            <!-- StatPerks -->
+                            <div class="flex gap-1 ml-2">
+                                <img src="${data.perks.offense}" class="w-6 h-6" alt="offense-perk">
+                                <img src="${data.perks.flex}" class="w-6 h-6" alt="flex-perk">
+                                <img src="${data.perks.defense}" class="w-6 h-6" alt="defense-perk">
+                            </div>
+                        </div>
+
+                        <!-- Itens e Spells - Quinta linha -->
+                        <div class="flex justify-between items-center mt-2">
+                            <!-- Itens -->
+                            <div class="flex flex-wrap gap-1">
+                                ${data.items
+                                .filter(id => id !== 0) // filtra itens "vazios"
+                                .map(id => `<img src="https://ddragon.leagueoflegends.com/cdn/15.8.1/img/item/${id}.png" class="w-6 h-6" alt="item ${id}">`)
+                                .join('')}
+                            </div>
+
+                            <!-- Spells -->
+                            <div class="flex gap-1 ml-2">
+                                <img src="${data.summonerSpells.spell1}" class="w-6 h-6 bg-black/25 backdrop-blur-sm" alt="spell1">
+                                <img src="${data.summonerSpells.spell2}" class="w-6 h-6 bg-black/25 backdrop-blur-sm" alt="spell2">
+                            </div>                    
+                            
+                        </div>
+
+
+                        <!-- Data da partida -->
+                        <div class="flex justify-between items-center mt-2">
+                            <!-- Achievements -->
+                            <div class="flex items-center">
+                                ${achievements.join("")}    
+                            </div>
+                                    
+                            <div class="mt-2 flex">                    
+                                <p style="color=white; -webkit-text-stroke: 0.1px silver;" class="text-sm text-gray-200 inline-flex px-2 py-1 rounded">${data.gameDate}</p>                    
+                            </div>
+                        </div>   
+
+                    </div>
                 </div>
 
-
-                <!-- Data da partida -->
-                <div class="flex justify-between items-center mt-2">
-                    <!-- Achievements -->
-                    <div class="flex items-center">
-                        ${achievements.join("")}    
-                    </div>
-                            
-                    <div class="mt-2 flex">                    
-                        <p style="color=white; -webkit-text-stroke: 0.1px silver;" class="text-sm text-gray-200 inline-flex px-2 py-1 rounded">${data.gameDate}</p>                    
-                    </div>
-                </div>   
+                <!-- Verso do card -->                
+                <div class="flip-back bg-center bg-cover text-white flex flex-col items-center justify-center border-4"
+                style="background-image: url('../assets/img/back-yugioh.jpg'); border-color: ${data.corDaBorda};">
+                     
+                </div>
 
             </div>
         </div>
     `;
 
-    
+    // Adiciona o evento de clique para virar
+    card.addEventListener("click", () => {
+        const inner = card.querySelector(".flip-inner");
+        inner.classList.toggle("rotate-y-180");
+    });
+
     container.appendChild(card);
+
+    VanillaTilt.init(card.querySelectorAll(".carta"), {
+        max: 2,
+        speed: 100,
+        glare: false,
+        reverse: true,
+        // "max-glare": 0.1,
+    });
+
 }
 
 function formatTime(seconds) {
